45ac8a91f2523e24b9ce5777355ccfe18a0396f2
Cara Brook <cbrook04@gmail.com>
2020-10-23 09:32:37 -0700

    adding Fig 3 symptom files

---
+++ all-model-runs/Fig3-Testing-vs-Symptom/Fig3-Symptom-Iso-Lags/RunScripts.sh
@@ -0,0 +1,5 @@
+R CMD BATCH --no-save symptom-iso-lag-1.R
+R CMD BATCH --no-save symptom-iso-lag-2.R
+R CMD BATCH --no-save symptom-iso-lag-3.R
+R CMD BATCH --no-save symptom-iso-lag-4.R
+R CMD BATCH --no-save symptom-iso-lag-5.R
