8c015681c140db916e5596163fcd41e3510d6c97
Cara Brook <cbrook04@gmail.com>
2020-12-29 20:59:56 -0800

    removing all-model-runs

--- all-model-runs/FigS3-Contact-Trace-Enhancement/symptom/RunScripts.sh
+++
@@ -1,5 +0,0 @@
-R CMD BATCH --no-save symptom-iso-lag-1-trace.R
-R CMD BATCH --no-save symptom-iso-lag-2-trace.R
-R CMD BATCH --no-save symptom-iso-lag-3-trace.R
-R CMD BATCH --no-save symptom-iso-lag-4-trace.R
-R CMD BATCH --no-save symptom-iso-lag-5-trace.R
