d77da03d31d0434a1723a3a3b5beba59900eeb83
Cara Brook <cbrook04@gmail.com>
2020-12-29 21:02:48 -0800

    Fig2 scripts

---
+++ all-model-runs/Fig2-Group-Limits/helper.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH --job-name=group-lim
+#SBATCH --account=fc_sirmodel
+#SBATCH --partition=savio3
+#SBATCH --ntasks-per-node=6
+#SBATCH --nodes=1
+#SBATCH --time=48:00:00
+module load r/3.6.3
+module load r-packages
+ht_helper.sh -m "r" -t RunScripts.sh -p 6
